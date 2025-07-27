9c1d0232832ad5088faf39f79319d6a5d7392c50
chaimatanabene <tanabenechaima@gmail.com>
2025-07-27 01:15:15 +0100

    Add files via upload

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request)
+    {
+        \Log::info('Signup endpoint hit:', $request->all());
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+        'name' => $request->username,
+        'username' => $request->username,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+]);
+
+
+        return response()->json(['message' => 'User successfully registered'], 201);
+    }
+
+    public function login(Request $request)
+{
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required|string',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['error' => 'Invalid credentials'], 401);
+    }
+
+    return response()->json([
+        'message' => 'Login successful',
+        'user' => $user,
+    ]);
+}
+
+}
+
---
+++ database/migrations/2025_07_26_230415_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('tasks', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->text('description');
+        $table->string('category'); // e.g. Personnel, Travail
+        $table->string('priority'); // e.g. Haute, Moyenne, Basse
+        $table->string('status');   // e.g. À faire, En cours
+        $table->date('deadline')->nullable();
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
